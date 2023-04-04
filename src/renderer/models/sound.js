6cff37cd7bf59c10888af4dd472e7b43a5442d35
Yusen Wu <sanmusen_wu@outlook.com>
2023-04-05 02:08:01 +0800

    chore：Facing Object

---
+++ src/renderer/models/sound.js
@@ -0,0 +1,36 @@
+/**
+ * 音效对象
+ */
+class Sound{
+    constructor(){
+        this.bgmNickName=""
+        this.bgmSourceName=""
+        this.bgmState=0//1or0
+        this.bgmVoice=1
+
+        this.soundEffectNickName=""
+        this.soundEffectSourceName=""
+        this.soundEffectState=0//1or0
+        this.soundEffectVoice=1
+    }
+
+    /**
+     * 解析块，返回用来 设置bgm 的脚本
+     * @param version 版本
+     * @param nickname 素材昵称
+     * @param sourcename 素材文件名
+     */
+    static setbgm_str(version,nickname,sourcename){
+        // TODO: 返回脚本字符串
+        return 
+    }
+
+    setbgm_obj(version,sentence="",nickname="",sourcename=""){
+        if(sentence){
+            // TODO: 用正则解析
+        }else if(nickname && sourcename){
+            this.bgmNickName=nickname
+            this.bgmSourceName=sourcename
+        }
+    }
+}
